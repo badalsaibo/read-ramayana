60eacc904d2dec2b78ed8653178940fa56b90551
Badal Saibo <badalsaibo@gmail.com>
2023-03-06 17:39:30 +0530

    feat: button

---
+++ src/components/Kanda/Kanda.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@mui/joy';
+
+type TKandaProps = {
+  children: React.ReactNode;
+};
+
+const Kanda = ({ children }: TKandaProps) => {
+  return <Button></Button>;
+};
+
+export default Kanda;
