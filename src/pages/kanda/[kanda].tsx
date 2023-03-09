32b36f56793c580b9b4031e3864d9a9b4dfb15b9
badalsaibo <badalsaibo@gmail.com>
2023-03-10 00:22:34 +0530

    feat: type

--- src/pages/kanda/[kanda].tsx
+++ src/pages/kanda/[kanda].tsx
@@ -1,5 +1,6 @@
 import { Typography } from '@mui/joy';
 import { GetStaticProps } from 'next';
+import { ParsedUrlQuery } from 'querystring';
 
 const Kanda = () => {
   return <Typography>Hello, Kanda</Typography>;
@@ -18,8 +19,12 @@ const getAllChaptersOfKanda = (kanda: string) => {
   // node library to fetch kanda
 };
 
+interface Params extends ParsedUrlQuery {
+  kanda: string;
+}
+
 export const getStaticProps: GetStaticProps = async (context) => {
-  const { kanda } = context.params;
+  const { kanda } = context.params as Params;
 
   const chapters = getAllChaptersOfKanda(kanda);
 
