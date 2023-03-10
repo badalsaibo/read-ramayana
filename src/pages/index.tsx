618004657df29ad89b19f0f1f98a060a47a05e56
Badal Saibo <badalsaibo@gmail.com>
2023-03-10 12:00:09 +0530

    feat: minor changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Grid from '@mui/joy/Grid';
 import Typography from '@mui/joy/Typography';
 import PaddedButton from 'components/PaddedButton';
+import { Box, Divider, Stack } from '@mui/joy';
 
 const KANDAS = [
   { kanda: 'bala' },
@@ -27,18 +28,23 @@ export default function Home() {
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="language" content="English" />
       </Head>
-      <Typography level="h1">Heading1</Typography>
-      <Typography level="body1">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe ad, molestias tenetur quam ipsa nostrum hic autem
-        quaerat harum at aspernatur, repellendus illo iusto soluta tempora. Fugit, numquam! Illum, dolore.
-      </Typography>
-      <Grid container spacing={2}>
-        {KANDAS.map(({ kanda }) => (
-          <Grid key={kanda} xs={6}>
-            <PaddedButton>{kanda}</PaddedButton>
+      <Stack spacing={1}>
+        <Typography level="body1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe ad, molestias tenetur quam ipsa nostrum hic
+          autem quaerat harum at aspernatur, repellendus illo iusto soluta tempora. Fugit, numquam! Illum, dolore.
+        </Typography>
+        <Typography level="h1">Kandas</Typography>
+        <Divider sx={{ mb: 1 }} />
+        <Box>
+          <Grid container spacing={2}>
+            {KANDAS.map(({ kanda }) => (
+              <Grid key={kanda} xs={6}>
+                <PaddedButton>{kanda}</PaddedButton>
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
+        </Box>
+      </Stack>
     </>
   );
 }
