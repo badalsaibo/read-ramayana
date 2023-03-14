b7397e139a1005bcd4bb135c7f178cba99879bc4
badalsaibo <badalsaibo@gmail.com>
2023-03-14 21:59:06 +0530

    feat: hydration resolver

---
+++ src/components/HydrationResolver/HydrationResolver.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+type HydrationResolverProps = {
+  children: React.ReactNode;
+};
+
+export default function HydrationResolver({
+  children,
+}: HydrationResolverProps) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return <>{children}</>;
+}
