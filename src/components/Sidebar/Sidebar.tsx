77ec1a8a6d0c8c98f3234f4521297a22d5bd5d00
badalsaibo <badalsaibo@gmail.com>
2023-03-28 01:35:07 +0530

    feat: update joy ui

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,6 +1,7 @@
 import { List, ListItem, Typography, Sheet, Button, ListItemButton, ListItemDecorator } from '@mui/joy';
 import Stack from '@mui/joy/Stack';
 import { KANDAS } from 'constant/kanda';
+import useKanda from 'hooks/provider/useKanda';
 import Hanuman from 'icons/Hanuman';
 import Link from 'next/link';
 
@@ -20,17 +21,36 @@ const IconsMap = {
 };
 
 const Sidebar = () => {
+  const { kanda: kandaRoot, setKanda } = useKanda();
   return (
-    <Stack>
-      <List>
+    <Sheet sx={{ p: 1 }} variant="soft" color="primary">
+      <List sx={{ '--ListItem-radius': '8px', '--List-gap': '12px' }}>
         {KANDAS.map(({ kanda, url }) => (
           <ListItem key={url}>
-            <ListItemButton component={Link} href={`/kanda/${url}`}>
-              <ListItemDecorator>{IconsMap[kanda]}</ListItemDecorator>
+            <ListItemButton
+              // component={Link}
+              // href={`/kanda/${url}`}
+              sx={
+                {
+                  // '&:not(.Joy-Selected)': {
+                  //   color: (theme) => theme.vars.palette.primary[200],
+                  // },
+                  // '&:hover': {
+                  //   color: (theme) => theme.vars.palette.primary[100],
+                  //   backgroundColor: (theme) => `rgba(${theme.vars.palette.primary.mainChannel} / 0.15)`,
+                  // },
+                }
+              }
+              selected={kanda === kandaRoot}
+              variant={kanda === kandaRoot ? 'solid' : 'plain'}
+              onClick={() => setKanda(kanda)}
+              color="primary"
+            >
+              <ListItemDecorator sx={{ color: 'inherit' }}>{IconsMap[kanda]}</ListItemDecorator>
               <Typography
                 textTransform="capitalize"
                 fontWeight={400}
-                sx={{ fontFamily: 'var(--joy-fontFamily-display)' }}
+                sx={{ fontFamily: 'var(--joy-fontFamily-display)', color: 'inherit' }}
               >
                 {kanda}
               </Typography>
@@ -38,7 +58,7 @@ const Sidebar = () => {
           </ListItem>
         ))}
       </List>
-    </Stack>
+    </Sheet>
   );
 };
 
