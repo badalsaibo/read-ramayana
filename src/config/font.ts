9337110de88637d6642da373ac91c97586747e07
Badal Saibo <badalsaibo@gmail.com>
2023-03-30 15:11:51 +0530

    refactor

--- src/config/font.ts
+++ src/config/font.ts
@@ -1,8 +1,6 @@
 import { Hind_Siliguri, Kumbh_Sans } from '@next/font/google';
 
-// const display = Halant({ subsets: ['latin'], weight: ['400', '500', '700'] });
-const display = Kumbh_Sans({ subsets: ['latin'], weight: ['400', '500', '700'] });
 const body = Hind_Siliguri({ subsets: ['latin'], weight: ['400'] });
-// const body = Kumbh_Sans({ subsets: ['latin'], weight: ['400'] });
+const display = Kumbh_Sans({ subsets: ['latin'], weight: ['400', '500', '700'] });
 
 export { body, display };
