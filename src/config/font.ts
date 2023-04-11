a3e0fbf79f50c220a224e57084e519da211b0699
Badal Saibo <badalsaibo@gmail.com>
2023-04-11 17:19:29 +0530

    feat: display swap

--- src/config/font.ts
+++ src/config/font.ts
@@ -1,6 +1,6 @@
 import { Hind_Siliguri, Kumbh_Sans } from '@next/font/google';
 
-const body = Hind_Siliguri({ subsets: ['latin'], weight: ['400'] });
-const display = Kumbh_Sans({ subsets: ['latin'], weight: ['400', '500', '700'] });
+const body = Hind_Siliguri({ subsets: ['latin'], weight: ['400'], display: 'swap' });
+const display = Kumbh_Sans({ subsets: ['latin'], weight: ['400', '500', '700'], display: 'swap' });
 
 export { body, display };
