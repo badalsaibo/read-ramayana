bd6e65af801cafdefa113c8d1331105838e3fdaa
Badal Saibo <badalsaibo@gmail.com>
2025-11-27 23:07:35 +0530

    fix: meta tags title

--- src/components/GenericHead/GenericHead.tsx
+++ src/components/GenericHead/GenericHead.tsx
@@ -4,8 +4,14 @@ const GenericHead = ({ title = 'Read Ramayana', description }: { title: string; 
   return (
     <Head>
       <title>{title}</title>
-      <meta property="og:title" name="title" content={title} />
-      <meta property="og:description" name="description" content={description} />
+
+      {/* normal meta */}
+      <meta name="description" content={description} />
+
+      {/* OG meta */}
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+
       <meta
         name="keywords"
         content="ramayana, valmiki, hinduism, india, ram, sita, lakshman, culture, heritage, ramayan, hanuman, bharat, mahabharata, epic, itihasa"
@@ -18,3 +24,5 @@ const GenericHead = ({ title = 'Read Ramayana', description }: { title: string; 
 };
 
 export default GenericHead;
+
+
