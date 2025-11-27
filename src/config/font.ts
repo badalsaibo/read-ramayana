1997d5a03c26930ccce337b6c9c396845317ad41
Badal Saibo <badalsaibo@gmail.com>
2025-11-27 23:27:30 +0530

    feat: with next/fonts

--- src/config/font.ts
+++ src/config/font.ts
@@ -1,4 +1,4 @@
-import { Hind_Siliguri, Kumbh_Sans } from '@next/font/google';
+import { Hind_Siliguri, Kumbh_Sans } from "next/font/google";
 
 const body = Hind_Siliguri({ subsets: ['latin'], weight: ['400'], display: 'swap' });
 const display = Kumbh_Sans({ subsets: ['latin'], weight: ['400', '500', '700'], display: 'swap' });
